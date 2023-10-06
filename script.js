9e99175cf01f4e02a146cb26a6d7b612a999916d
BHASVIC-rhysbonwick22 <137883343+BHASVIC-rhysbonwick22@users.noreply.github.com>
2023-10-06 13:01:08 +0000

    Added stack for stage 1

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 
+function LineEquation () {
+  var size = 10 ;
+  var stack = [] ;
+  var top = 0 ;
+  this.push = function (key) {
+    if (top != size) {
+      stack[top] = key ;
+      top ++ ;
+    }
+    else {
+      window.alert("Overflow error") ;
+    }
+  } 
+  this.pop = function () {
+    if (top != 0) {
+      top -- ;
+      return stack[top] ;
+    }
+    else {
+      window.alert("underflow error")
+    }
+  } 
+  this.display = function () {
+    for (let i = 0 ; i <= top ; i++) {
+      console.log(stack[i]) ;
+    }
+  } 
+}
+  const testEquation = new LineEquation() ;
+  testEquation.push("hello") ;
+  testEquation.push("yes") ;
+  testEquation.pop() ;
+  testEquation.push("why") ;
+  testEquation.display() 
+  console.log(testEquation.top) ;
+
+function lineList () {
+  var currentLine = 0 ;
+  var list = [] ;
+  this.addline = function (newLine) {
+    list[index] = newLine ;
+  }
+  this.removeline = function (index) {
+  }
+}
